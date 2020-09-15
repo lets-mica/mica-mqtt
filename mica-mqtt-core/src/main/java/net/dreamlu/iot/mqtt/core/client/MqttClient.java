c4b7ebdcb4380f9e60d15387bddab7d0fc63819e
如梦技术 <596392912@qq.com>
2020-09-15 21:35:52 +0800

    :sparkles: 梳理服务器和客户端的配置。

---
+++ mica-mqtt-core/src/main/java/net/dreamlu/iot/mqtt/core/client/MqttClient.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.net.dreamlu.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package net.dreamlu.iot.mqtt.core.client;
+
+/**
+ * mqtt 客户端
+ *
+ * @author L.cm
+ */
+public class MqttClient {
+
+	// 1. 服务端信息 ip、端口、timeout，考虑 ssl
+	// 2. 重连配置
+	// 3. 客户端信息 clientId、userName、pwd
+	// 4. 自定义消息处理
+	// 5. sub 的 topic 和 监听的配置
+
+}
