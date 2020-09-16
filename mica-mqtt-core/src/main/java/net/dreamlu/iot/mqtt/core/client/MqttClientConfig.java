25688b8e83d7765414ec119b514c42bc1f0fd418
如梦技术 <596392912@qq.com>
2020-09-16 22:00:10 +0800

    :sparkles: 完善 MqttClientConfig。

--- mica-mqtt-core/src/main/java/net/dreamlu/iot/mqtt/core/client/MqttClientConfig.java
+++ mica-mqtt-core/src/main/java/net/dreamlu/iot/mqtt/core/client/MqttClientConfig.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (c) 2019-2029, Dreamlu 卢春梦 (596392912@qq.com & www.net.dreamlu.net).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package net.dreamlu.iot.mqtt.core.client;
 
 import net.dreamlu.iot.mqtt.codec.MqttVersion;
+import org.tio.core.ssl.SslConfig;
 
 /**
  * MqttClient 配置
@@ -21,6 +38,10 @@ public class MqttClientConfig {
 	 * 超时时间，t-io 配置，可为 null
 	 */
 	private Integer timeout;
+	/**
+	 * SSL配置
+	 */
+	protected SslConfig sslConfig;
 	/**
 	 * 自动重连
 	 */
