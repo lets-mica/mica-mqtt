489275d92f3374f2b6449939525c93be37695de7
如梦技术 <596392912@qq.com>
2021-08-16 22:03:46 +0800

    :sparkles: 优化 deploy.sh 脚本。

--- deploy.sh
+++ deploy.sh
@@ -2,11 +2,11 @@
 
 ## 1. java version
 java -version
-echo "\n"
+printf "\n"
 
 ## 2. mvn version
 mvn -version
-echo "\n"
+printf "\n"
 
 ## 3. deploy
 mvn clean package deploy -Prelease -pl mica-mqtt-codec,mica-mqtt-core,mica-mqtt-spring-boot-starter
