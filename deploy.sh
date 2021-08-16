cb5757ead132388991b610adca36105f6df1d63d
如梦技术 <596392912@qq.com>
2021-08-16 21:40:34 +0800

    :bookmark: 1.0.5

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,12 @@
 #!/bin/sh
 
+## 1. java version
+java -version
+echo "\n"
+
+## 2. mvn version
+mvn -version
+echo "\n"
+
+## 3. deploy
 mvn clean package deploy -Prelease -pl mica-mqtt-codec,mica-mqtt-core,mica-mqtt-spring-boot-starter
