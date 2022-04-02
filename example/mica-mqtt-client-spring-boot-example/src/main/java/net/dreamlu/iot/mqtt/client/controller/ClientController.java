c185198bff0aa300416816bcefc8a72d29c19012
如梦技术 <596392912@qq.com>
2022-04-02 14:15:11 +0800

    :arrow_up: 依赖升级.

--- example/mica-mqtt-client-spring-boot-example/src/main/java/net/dreamlu/iot/mqtt/client/controller/ClientController.java
+++ example/mica-mqtt-client-spring-boot-example/src/main/java/net/dreamlu/iot/mqtt/client/controller/ClientController.java
@@ -1,11 +1,12 @@
 package net.dreamlu.iot.mqtt.client.controller;
 
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.dreamlu.iot.mqtt.client.service.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-@Api(tags = "Mqtt::客户端")
+@Tag(name = "Mqtt::客户端")
 @RequestMapping("/mqtt/client")
 @RestController
 public class ClientController {
@@ -13,11 +14,13 @@ public class ClientController {
 	@Autowired
 	private ClientService service;
 
+	@Operation(summary = "publish")
 	@PostMapping("/publish")
 	public boolean publish(@RequestBody String body) {
 		return service.publish(body);
 	}
 
+	@Operation(summary = "sub")
 	@GetMapping("/sub")
 	public boolean sub() {
 		return service.sub();
--- example/mica-mqtt-server-spring-boot-example/src/main/java/net/dreamlu/iot/mqtt/server/controller/ServerController.java
+++ example/mica-mqtt-server-spring-boot-example/src/main/java/net/dreamlu/iot/mqtt/server/controller/ServerController.java
@@ -1,6 +1,7 @@
 package net.dreamlu.iot.mqtt.server.controller;
 
-import io.swagger.annotations.Api;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import net.dreamlu.iot.mqtt.server.service.ServerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,13 +9,14 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Api(tags = "Mqtt::服务端")
+@Tag(name = "Mqtt::服务端")
 @RequestMapping("/mqtt/server")
 @RestController
 public class ServerController {
 	@Autowired
 	private ServerService service;
 
+	@Operation(summary = "publish")
 	@PostMapping("publish")
 	public boolean publish(@RequestBody String body) {
 		return service.publish(body);
