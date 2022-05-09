2f2268fcb6df42d4891450490574ff263c4ccc2f
浅梦 <1101766085@qq.com>
2022-05-09 18:04:48 +0800

    :sparkles: mqtt topic 添加测试场景。

--- mica-mqtt-core/src/test/java/net/dreamlu/iot/mqtt/core/util/TopicUtilTest.java
+++ mica-mqtt-core/src/test/java/net/dreamlu/iot/mqtt/core/util/TopicUtilTest.java
@@ -47,6 +47,11 @@ public class TopicUtilTest {
 		Assert.assertFalse(TopicUtil.match("/iot/+/+/test", "/iot/abc/123/test/"));
 		Assert.assertTrue(TopicUtil.match("/iot/+/+/+", "/iot/abc/123/test"));
 		Assert.assertFalse(TopicUtil.match("/iot/+/+/+", "/iot/abc/123/test/"));
+		Assert.assertTrue(TopicUtil.match("/iot/+/test", "/iot/a/test"));
+		Assert.assertTrue(TopicUtil.match("/iot/+/test", "/iot/a/test"));
+		Assert.assertFalse(TopicUtil.match("/iot/+/+/+", "/iot/a//test/"));
+		Assert.assertFalse(TopicUtil.match("/iot/+/+/+", "/iot/a/b/c/"));
+		Assert.assertFalse(TopicUtil.match("/iot/+/+/+", "/iot/a"));
 
 		Assert.assertTrue(TopicUtil.match("#", "/iot/test"));
 		Assert.assertTrue(TopicUtil.match("/iot/test/#", "/iot/test"));
