3e0e024dca02a72cf9afe69ae977fcd697a60838
如梦技术 <596392912@qq.com>
2022-05-22 14:35:45 +0800

    :sparkles: TimingWheel 完善，待添加进业务。

--- mica-mqtt-core/src/test/java/net/dreamlu/iot/mqtt/core/timer/SystemTimerTest.java
+++ mica-mqtt-core/src/test/java/net/dreamlu/iot/mqtt/core/timer/SystemTimerTest.java
@@ -1,22 +1,23 @@
 package net.dreamlu.iot.mqtt.core.timer;
 
 import net.dreamlu.iot.mqtt.core.util.timer.SystemTimer;
+import net.dreamlu.iot.mqtt.core.util.timer.TimingWheelThread;
 
 public class SystemTimerTest {
-    public static void main(String[] args) {
-        SystemTimer systemTimer = new SystemTimer("timer");
-        System.out.println(System.currentTimeMillis());
-        systemTimer.add(new DelayedOperation(5000));
-        systemTimer.add(new DelayedOperation(7000));
-        systemTimer.add(new DelayedOperation(10000));
-        systemTimer.add(new DelayedOperation(14000));
-        System.out.println(System.nanoTime());
-        boolean flag = true;
-        while (flag) {
-            boolean b = systemTimer.advanceClock(200);
-            if (b) {
-                System.out.println(systemTimer.size());
-            }
-        }
-    }
+
+	public static void main(String[] args) throws InterruptedException {
+		SystemTimer systemTimer = new SystemTimer("timer");
+
+		TimingWheelThread timingWheelThread = new TimingWheelThread(systemTimer);
+		timingWheelThread.start();
+
+		System.out.println(System.currentTimeMillis());
+		systemTimer.add(new DelayedOperation(5000));
+		systemTimer.add(new DelayedOperation(7000));
+		systemTimer.add(new DelayedOperation(10000));
+		systemTimer.add(new DelayedOperation(14000));
+		System.out.println(System.nanoTime());
+
+		Thread.sleep(100000L);
+	}
 }
