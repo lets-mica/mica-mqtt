d8033838397946ede09c9a2cfdab80ed2f4ca459
浅梦 <1101766085@qq.com>
2022-05-26 19:40:17 +0800

    :sparkles: 添加 Compression 压缩接口。

---
+++ mica-mqtt-core/src/test/java/net/dreamlu/iot/mqtt/core/util/CompressionTest.java
@@ -0,0 +1,27 @@
+package net.dreamlu.iot.mqtt.core.util;
+
+import net.dreamlu.iot.mqtt.core.util.compression.GzipCompression;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * Compression 测试
+ *
+ * @author L.cm
+ */
+public class CompressionTest {
+
+	@Test
+	public void test() throws IOException {
+		String text = "如梦技术——你的编程加油站！";
+		GzipCompression compression = new GzipCompression();
+		byte[] bytes = compression.compress(text.getBytes(StandardCharsets.UTF_8));
+		byte[] decompress = compression.decompress(bytes);
+		String s = new String(decompress, StandardCharsets.UTF_8);
+		Assert.assertEquals(text, s);
+	}
+
+}
