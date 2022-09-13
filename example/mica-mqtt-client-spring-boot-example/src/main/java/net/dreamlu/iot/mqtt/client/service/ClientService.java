10edf635ccb8371bf25a6edb4bcd481a9ef9ae15
浅梦 <1101766085@qq.com>
2022-09-13 14:58:55 +0800

    :sparkles: 开始 2.0.2-SNAPSHOT

--- example/mica-mqtt-client-spring-boot-example/src/main/java/net/dreamlu/iot/mqtt/client/service/ClientService.java
+++ example/mica-mqtt-client-spring-boot-example/src/main/java/net/dreamlu/iot/mqtt/client/service/ClientService.java
@@ -7,7 +7,6 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.nio.ByteBuffer;
 import java.nio.charset.StandardCharsets;
 
 /**
