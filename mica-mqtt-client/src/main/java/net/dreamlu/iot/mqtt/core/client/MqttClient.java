ae594840026367f856f140d306fcef25ede5508b
浅梦 <1101766085@qq.com>
2024-01-12 17:53:24 +0800

    :sparkles: 修复判断问题

--- mica-mqtt-client/src/main/java/net/dreamlu/iot/mqtt/core/client/MqttClient.java
+++ mica-mqtt-client/src/main/java/net/dreamlu/iot/mqtt/core/client/MqttClient.java
@@ -538,7 +538,7 @@ public final class MqttClient {
 	 */
 	public boolean isConnected() {
 		ClientChannelContext channelContext = getContext();
-		return channelContext != null && !channelContext.isAccepted();
+		return channelContext != null && channelContext.isAccepted();
 	}
 
 	/**
