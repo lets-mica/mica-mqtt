a30bdc250839ee22898b01cbb983c15205ab298a
如梦技术 <596392912@qq.com>
2024-07-30 22:59:16 +0800

    :sparkles: 代码优化

--- starter/mica-mqtt-server-solon-plugin/src/main/java/net/dreamlu/iot/mqtt/server/noear/config/DataSize.java
+++ starter/mica-mqtt-server-solon-plugin/src/main/java/net/dreamlu/iot/mqtt/server/noear/config/DataSize.java
@@ -15,7 +15,7 @@ import java.util.regex.Pattern;
  */
 @Getter
 @RequiredArgsConstructor
-public class DataSize {
+class DataSize {
 
 	/**
 	 * Bytes per Kilobyte.
@@ -38,10 +38,6 @@ public class DataSize {
 	private static final long BYTES_PER_TB = BYTES_PER_GB * 1024;
 
 	private final long bytes;
-
-	public DataSize(String text) {
-		this(parse(text).getBytes());
-	}
 
 	/**
 	 * Obtain a {@link DataSize} representing the specified number of bytes.
@@ -144,6 +140,56 @@ public class DataSize {
 		private static DataUnit determineDataUnit(String suffix) {
 			return (StrUtil.isNotBlank(suffix) ? DataUnit.fromSuffix(suffix) : DataUnit.BYTES);
 		}
-
 	}
+
+	@Getter
+	@RequiredArgsConstructor
+	public enum DataUnit {
+
+		/**
+		 * Bytes, represented by suffix {@code B}.
+		 */
+		BYTES("B", DataSize.ofBytes(1)),
+
+		/**
+		 * Kilobytes, represented by suffix {@code KB}.
+		 */
+		KILOBYTES("KB", DataSize.ofKilobytes(1)),
+
+		/**
+		 * Megabytes, represented by suffix {@code MB}.
+		 */
+		MEGABYTES("MB", DataSize.ofMegabytes(1)),
+
+		/**
+		 * Gigabytes, represented by suffix {@code GB}.
+		 */
+		GIGABYTES("GB", DataSize.ofGigabytes(1)),
+
+		/**
+		 * Terabytes, represented by suffix {@code TB}.
+		 */
+		TERABYTES("TB", DataSize.ofTerabytes(1));
+
+
+		private final String suffix;
+		private final DataSize size;
+
+		/**
+		 * Return the {@link DataUnit} matching the specified {@code suffix}.
+		 * @param suffix one of the standard suffixes
+		 * @return the {@link DataUnit} matching the specified {@code suffix}
+		 * @throws IllegalArgumentException if the suffix does not match the suffix
+		 * of any of this enum's constants
+		 */
+		public static DataUnit fromSuffix(String suffix) {
+			for (DataUnit candidate : values()) {
+				if (candidate.suffix.equals(suffix)) {
+					return candidate;
+				}
+			}
+			throw new IllegalArgumentException("Unknown data unit suffix '" + suffix + "'");
+		}
+	}
+
 }
